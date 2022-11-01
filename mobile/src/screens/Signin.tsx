12783eac815300f1cb842b7e3baa96bb4ba04ff9
Jeziel Almeida <jeziel.almeida16@gmail.com>
2022-11-01 15:34:52 +0000

    Projeto Mobile - React Native adicionado

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,15 @@
+import { Text, Center } from 'native-base';
+
+
+export function Signin() {
+
+    return (
+        <Center flex={1} bgColor="gray.900">
+
+            <Text color="white" fontSize={24} fontFamily="heading">
+                Sign in
+            </Text>
+
+        </Center>
+    )
+}
