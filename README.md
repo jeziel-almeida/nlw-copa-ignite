7bc6e55a933a5f42fc99244e35f15ad0efcb04cf
Jeziel Almeida <jeziel.almeida16@gmail.com>
2022-11-02 04:52:49 +0000

    Aula 02 Concluída

---
+++ README.md
@@ -0,0 +1,3 @@
+## NLW COPA IGNITE
+
+![Logo](./web/src/assets/wallpaper.png)
---
+++ web/src/lib/axios.ts
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const api = axios.create({
+    baseURL: 'https://3333-jezielalmei-nlwcopaigni-9mui4h650fd.ws-us73.gitpod.io/'
+})
