7bba6c176f0bfc3598bfa45d3cd93969b8103e1a
Jeziel Almeida <jeziel.almeida16@gmail.com>
2022-11-02 15:42:38 +0000

    Interface de Autenticação concluída

--- mobile/src/screens/Signin.tsx
+++ mobile/src/screens/Signin.tsx
@@ -1,13 +1,26 @@
-import { Text, Center } from 'native-base';
+import { Text, Center, Icon } from 'native-base';
+import { Fontisto } from '@expo/vector-icons'
 
+import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button';
 
 export function Signin() {
 
     return (
-        <Center flex={1} bgColor="gray.900">
+        <Center flex={1} bgColor="gray.900" p={7}>
 
-            <Text color="white" fontSize={24} fontFamily="heading">
-                Sign in
+            <Logo width={212} height={40} />
+
+            <Button 
+                type="SECONDARY"
+                title="ENTRAR COM O GOOGLE" 
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+                mt={12}
+            />
+
+            <Text color="white" textAlign="center" mt={4}>
+                Não utilizamos nenhuma informação além {'\n'}
+                do seu email para criação da sua conta.
             </Text>
 
         </Center>
