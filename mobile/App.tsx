f04293a497eb0dcd04fba0876c889835ae70a38e
Jeziel Almeida <jeziel.almeida16@gmail.com>
2022-11-02 21:07:37 -0300

    Components e screens New, Find e Pools criadas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,8 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { THEME } from './src/styles/theme'
-import { Signin } from './src/screens/Signin'
+// import { Signin } from './src/screens/Signin'
+import { New } from './src/screens/New'
 import { Loading } from './src/components/Loading'
 import { AuthContextProvider } from './src/contexts/AuthContext';
 
@@ -21,7 +22,7 @@ export default function App() {
         />
 
         {
-          fontsLoaded ? <Signin /> : <Loading />
+          fontsLoaded ? <New /> : <Loading />
         }
       </AuthContextProvider>
     </NativeBaseProvider>
